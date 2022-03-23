61fb13ed2c4c81d56a13c46f2f3c80d2ef8aacc5
student <228870@edu.p.lodz.pl>
2022-03-23 09:51:56 +0100

    addded division

--- task1.py
+++ task1.py
@@ -3,3 +3,9 @@ print("Developer Vermilion")
 print("Developer Panzer0")
 print("GROMoOS")
 print("209424")
+
+
+def Division(A, B):
+    if B == 0:
+        return "Illegal operation"
+    return A / B
