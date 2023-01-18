a526c3742d806945ed53f1a8d3a7de1714042b04
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2023-01-18 23:29:16 +0100

    moved functions into new files, added test for reading base config file

--- src/main.py
+++ src/main.py
@@ -1,16 +1,4 @@
 import requests
-from datetime import date
-
-def date_to_iso8601(input_date: date) -> str:
-  return input_date.isoformat()
-
-headers = {
-  "Accept": "application/json"
-}
-
-URL = "http://api.nbp.pl/api"
-
-START_DATE = "2002-01-02"
 
 response = requests.get(URL + f'/exchangerates/tables/A/{START_DATE}', headers=headers)
 
--- tests/test.py
+++ tests/test.py
@@ -1,8 +1,11 @@
 import unittest
 from datetime import date
-from src.main import date_to_iso8601
+from src.functions import *
 
 class TestApp(unittest.TestCase):
   def test_convert_data(self):
     input_date = date(1996, 6, 11)
     self.assertEqual(date_to_iso8601(input_date), "1996-06-11")
+
+  def test_load_config(self):
+    self.assertEqual("http://api.nbp.pl/api", load_config("src/config.json")["URL"])
