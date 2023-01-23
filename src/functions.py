6e2b0911ea0c81a07aa87d03b333f97b19b1be56
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2023-01-23 22:18:45 +0100

    red test

--- src/functions.py
+++ src/functions.py
@@ -9,3 +9,9 @@ def date_to_iso8601(input_date: date) -> str:
 def load_config(dir: str) -> dict:
     with open(dir) as file:
         return json.load(file)
+
+def get_all_currencies_codes(data: dict)-> list:
+    return list(map(lambda x: x["code"],data[0]["rates"]))
+
+def count_tendency_hist(data: dict, currency: str) -> dict:
+    pass
--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,8 @@
-import unittest
+import unittest, json, random
 from datetime import date
 from src.functions import *
 
+TEST_JSON = "tests/2022-12-31-to-2023-01-18.json"
 
 class TestApp(unittest.TestCase):
     def test_convert_data(self):
@@ -10,3 +11,11 @@ class TestApp(unittest.TestCase):
 
     def test_load_config(self):
         self.assertEqual("http://api.nbp.pl/api", load_config("src/config.json")["URL"])
+
+    def test_count_tendency_hist_should_return_three_categories(self):
+        with open(TEST_JSON, "rt") as file:
+            data = json.load(file)
+            currency = random.choice(get_all_currencies_codes(data))
+            hist = count_tendency_hist(data, currency)
+            for elem in ("increase", "same", "decrease"):
+                self.assertTrue(elem in hist.keys())
