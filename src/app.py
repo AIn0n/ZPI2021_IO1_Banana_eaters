e2f169c6023608a9e94c66600c430d53a470d3d9
Szymon Górka <55995692+AIn0n@users.noreply.github.com>
2023-02-06 19:38:23 +0100

    Moves different stats into expanders

--- src/app.py
+++ src/app.py
@@ -17,69 +17,67 @@ data_load_state = st.text("loading data")
 data = test_loading_data()
 data_load_state.text("data successfully fetched from API")
 
-st.subheader("single currency analysis")
+with st.expander("single currency analysis"):
+    currencies = get_all_currencies_codes(data)
+    selected = st.selectbox("select currency", currencies, key=0)
 
-currencies = get_all_currencies_codes(data)
-selected = st.selectbox("select currency", currencies, key=0)
+    # get days period
+    st.subheader(":bar_chart: Histogram of changes")
+    hist_time_dict = {"week": 7, "two weeks": 14, "month": 30, "quarter": 90}
+    selected_time = st.selectbox("select time period", hist_time_dict.keys())
 
-# get days period
-st.subheader(":bar_chart: Histogram of changes")
-hist_time_dict = {"week": 7, "two weeks": 14, "month": 30, "quarter": 90}
-selected_time = st.selectbox("select time period", hist_time_dict.keys())
+    # prepare data and add chart
+    collection = count_tendency_hist(data, selected, hist_time_dict[selected_time])
+    df = pd.DataFrame.from_dict(collection, orient="index", columns=["number of days"])
+    fig = px.bar(df, x=df.index, y="number of days")
+    st.plotly_chart(fig)
 
-# prepare data and add chart
-collection = count_tendency_hist(data, selected, hist_time_dict[selected_time])
-df = pd.DataFrame.from_dict(collection, orient="index", columns=["number of days"])
-fig = px.bar(df, x=df.index, y="number of days")
-st.plotly_chart(fig)
+    st.subheader(":clipboard: statistics table")
 
-st.subheader(":clipboard: statistics table")
+    rates = get_rates(data, selected, hist_time_dict[selected_time])
+    mean = statistics.mean(rates)
+    stdev = statistics.stdev(rates)
+    stats = [
+        ["average", mean],
+        ["median", statistics.median(rates)],
+        ["standard deviation", stdev],
+        ["coefficient of variation", stdev / mean * 100],
+    ]
 
-rates = get_rates(data, selected, hist_time_dict[selected_time])
-mean = statistics.mean(rates)
-stdev = statistics.stdev(rates)
-stats = [
-    ["average", mean],
-    ["median", statistics.median(rates)],
-    ["standard deviation", stdev],
-    ["coefficient of variation", stdev / mean * 100],
-]
+    stats_df = pd.DataFrame(stats, columns=["function", "value"])
+    stats_df = stats_df.set_index("function").T
+    stats_df.columns = [
+        "average",
+        "median",
+        "standard deviation",
+        "coefficient of variation",
+    ]
 
-stats_df = pd.DataFrame(stats, columns=["function", "value"])
-stats_df = stats_df.set_index("function").T
-stats_df.columns = [
-    "average",
-    "median",
-    "standard deviation",
-    "coefficient of variation",
-]
+    st.table(stats_df)
 
-st.table(stats_df)
+with st.expander("two currencies comparison"):
+    left_col, mid_col, right_col = st.columns(3)
+    first_curr = second_curr = days = None
+    comp_time_dict = {"month": 30, "quarter": 90}
 
-st.subheader("two currencies comparison")
-st.text("choose currencies")
+    with left_col:
+        first_curr = st.selectbox("select first currency", currencies)
 
-left_col, mid_col, right_col = st.columns(3)
-first_curr = second_curr = days = None
-comp_time_dict = {"month": 30, "quarter": 90}
+    with mid_col:
+        second_curr = st.selectbox("select second currency", currencies)
 
-with left_col:
-    first_curr = st.selectbox("select first currency", currencies)
+    with right_col:
+        days = st.selectbox("choose time range", comp_time_dict.keys())
 
-with mid_col:
-    second_curr = st.selectbox("select second currency", currencies)
-
-with right_col:
-    days = st.selectbox("choose time range", comp_time_dict.keys())
-
-
-if first_curr is second_curr:
-    st.error("please choose two different currencies to compare")
-else:
-    first_rates = get_rates(data, first_curr, comp_time_dict[days])
-    second_rates = get_rates(data, second_curr, comp_time_dict[days])
-    diff = [a / b for a, b in zip(first_rates, second_rates)]
-    comp = prep_data_dist_of_changes(diff, 7)
-    comp_df = pd.DataFrame.from_dict(comp, orient="index", columns=["number of days"])
-    comp_fig = px.bar(comp_df, x=comp_df.index, y="number of days")
-    st.plotly_chart(comp_fig)
+    if first_curr is second_curr:
+        st.error("please choose two different currencies to compare")
+    else:
+        first_rates = get_rates(data, first_curr, comp_time_dict[days])
+        second_rates = get_rates(data, second_curr, comp_time_dict[days])
+        diff = [a / b for a, b in zip(first_rates, second_rates)]
+        comp = prep_data_dist_of_changes(diff, 9)
+        comp_df = pd.DataFrame.from_dict(
+            comp, orient="index", columns=["number of days"]
+        )
+        comp_fig = px.bar(comp_df, x=comp_df.index, y="number of days")
+        st.plotly_chart(comp_fig)
